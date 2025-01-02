a508784555c6965427f50467fec9ff3922eb36a7
Kanha Agrawal <kanha.agr11@gmail.com>
2025-01-02 13:10:14 +0000

    added basic css and html

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app =Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def home():
-  return "Hello world"
+  return render_template("index.html")
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
